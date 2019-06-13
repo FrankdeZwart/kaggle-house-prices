55221fab777a44be964473ba9c3ff1982b9cfcc5
Frank de Zwart <>
2019-06-13 15:51:55 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This challenge aims to predict interactions between atoms. Imaging technologies like MRI enable us to see and understand the molecular composition of tissues. Nuclear Magnetic Resonance (NMR) is a closely related technology which uses the same principles to understand the structure and dynamics of proteins and molecules.',
+    author='Milan & Frank',
+    license='',
+)
