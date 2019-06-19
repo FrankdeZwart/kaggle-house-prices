586efab4947c68cfd6ca62dcc7d4463020c2360b
Frank de Zwart <>
2019-06-19 17:12:00 +0200

    Updated requirements list

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='prices',
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas==0.24.2',
+    ],
     version='0.1.0',
-    description='This challenge aims to predict interactions between atoms. Imaging technologies like MRI enable us to see and understand the molecular composition of tissues. Nuclear Magnetic Resonance (NMR) is a closely related technology which uses the same principles to understand the structure and dynamics of proteins and molecules.',
+    description='This challenge aims to predict interactions between atoms. Imaging technologies '
+                'like MRI enable us to see and understand the molecular composition of tissues. '
+                'Nuclear Magnetic Resonance (NMR) is a closely related technology which uses the '
+                'same principles to understand the structure and dynamics of proteins and molecules.',
     author='Milan & Frank',
     license='',
+    scripts=['prices/bin/prices'],
+    data_files=[('prices_data', ['logger.ini'])]
 )
