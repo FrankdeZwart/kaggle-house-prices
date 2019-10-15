3ef7faa00e921fc8d982f446ae0dc4759d2f37af
Frank de Zwart <>
2019-10-15 10:16:31 +0200

    Fitted a random forrest regression

--- prices/__main__.py
+++ prices/__main__.py
@@ -8,9 +8,8 @@ import sys
 
 from prices.features.build_features import FeatureBuilder
 from prices.data.dataset_builder import DatasetBuilder
-# from prices.features.feature_selection import FeatureSelection
 from prices.models.linear_regression import LinearRegression
-# from prices.models.random_forest import RandomForest
+from prices.models.random_forest import RandomForest
 
 log_file_path = os.path.join(sys.prefix, 'prices_data', 'logger.ini')
 logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
@@ -53,38 +52,29 @@ def main():
 
     # Feature engineering
     X, y = FeatureBuilder(filename=args.train).build_features()
-    logger.info('Selected features are: ' + str(X.columns))
 
     # Stepwise feature building
-    print(LinearRegression().select_features_forward(X=X, y=y))
-
-    logger.info('Script succeeded.')
-
-
-"""
-# Stepwise feature building
-    feature_selection = LinearRegression().stepwise_feature_builder(y=y_train, X=X_train)
+    feature_selection = LinearRegression().select_features_forward(X=X, y=y)
     logger.info('Selected features are: ' + str(feature_selection['selected_features']))
 
     # Basic OLS regression
-    results_ols = LinearRegression().ols_basic(y=y_train, X=X_train[feature_selection['selected_features']])
+    results_ols = LinearRegression().ols_basic(y=y, X=X[feature_selection['selected_features']])
 
     # Random forest regression
-    results_rf = RandomForest().rf_basic(y_train=y_train, X_train=X_train[feature_selection['selected_features']])
+    results_rf = RandomForest().rf_basic(y_train=y, X_train=X[feature_selection['selected_features']])
 
-    # # Print rf tree
-    # RandomForest().rf_tree_output(mdl=results_rf['mdl'],
-    #                               feature_list=['Constant'] + feature_selection['selected_features'])
+    # Print rf tree
+    RandomForest().rf_tree_output(mdl=results_rf['mdl'],
+                                  feature_list=['Constant'] + feature_selection['selected_features'])
 
     # Evaluate performance
     evaluation_ols = LinearRegression().ols_performance(
         fit=results_ols['mdl'],
-        true_values=y_train,
+        true_values=y,
         pred_values=results_ols['is_fit'],
         n_features=results_ols['num_features'])
     logger.info('OLS in-sample RMSLE: %s', evaluation_ols['rmsle'])
-    evaluation_rf = RandomForest().rf_performance(true_values=y_train, pred_values=results_rf['is_fit'])
+    evaluation_rf = RandomForest().rf_performance(true_values=y, pred_values=results_rf['is_fit'])
     logger.info('Random Forest in-sample RMSLE: %s', evaluation_rf['rmsle'])
 
     logger.info('Script succeeded.')
-"""
